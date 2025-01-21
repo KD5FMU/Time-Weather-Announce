db92cfb1c12873d1c311dafc23da12f85b6dae4c
Jory Pratt <anarchy@gentoo.org>
2025-01-21 12:33:27 -0600

    Ensure a proper copyright and license to freely modify script
    Signed-off-by: Jory Pratt <anarchy@gentoo.org>

--- time_weather.sh
+++ time_weather.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
-
+#
 # Time and Weather Announcement Setup Script
-# Originally created by Freddie Mac - KD5FMU on November 17, 2024
-# This script automates the installation of tools for time and weather announcements.
+# Copyright (C) 2024 Freddie Mac - KD5FMU
+# Copyright (C) 2025 Jory A. Pratt - W5GLE
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/gpl-2.0.html>.
 
 # Check if the script is run as root
 if [ "$(id -u)" != "0" ]; then
